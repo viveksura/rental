ced2d22a85e6fcbee4064c123a6e5372ecda66a2
viveksura <emailviveksura@gmail.com>
2023-02-25 11:49:52 -0600

    add routes and views

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,24 +1,39 @@
 class PropertiesController < ApplicationController
     before_action :authenticate_user!
 
-    def index
+    def all
         if current_user.PropertyManager?
-            properties = Property.all
+            @properties = Property.all
         else
-            properties = Property.available_properties
+            redirect_to 404.html
+        end
+    end
+
+    def show
+        if current_user.Renter?
+           @property =  current_user&.current_rental_property
+        elsif current_user.PropertyManager?
+            @property = Property.where(id: params[:id]).first
         end
 
-        render json: properties
+        if @property & @property.id == params[:id].to_i
+            @current_occupancy = @property&.current_occupancy
+        else
+            redirect_to 404.html
+        end
+    end
+
+    def available
+        @properties = Property.available_properties
     end
 
     def update_rent
         property = Property.where(manager_id: current_user.id, id: params[:id]).first
         if property
             property.update(rent: params[:rent])
-            render status: 200
+            redirect_to property_show
         else
-            render status: 403
+            redirect_to 422.html
         end
     end
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,19 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "properties#index"
+
+
+  resources :properties, only: [] do
+    collection do
+      get "all"
+      get "available"
+    end
+
+    member do
+      get "show"
+      put "update_rent"
+    end
+  end
+
+  root "properties#available"
 end
