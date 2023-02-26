3ae134a19d51fb9de34144ed81a2f61541378161
viveksura <emailviveksura@gmail.com>
2023-02-26 12:18:00 -0600

    fix css issues

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,9 +3,9 @@ class AppointmentsController < ApplicationController
 
     def index
         if current_user.Renter?
-            @appointments = Appointment.where(renter_id: current_user.id).where("created_at >= ?", Date.today).includes(:renter, property: :manager)
+            @appointments = Appointment.where(renter_id: current_user.id).where("created_at >= ?", Date.today).includes(:renter, property: :manager).order("date asc")
         else 
-            @appointments = Appointment.where("created_at >= ?", Date.today).includes(:renter, property: :manager)
+            @appointments = Appointment.where("created_at >= ?", Date.today).includes(:renter, property: :manager).order("date asc")
         end
     end
 end
